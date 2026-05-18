7954856b913f10e676b234fc7dbea10e23048a6f
Windy <windynapitupulu3108@gmail.com>
2026-05-18 11:07:54 +0700

    Initial Commit

---
+++ BiteBeat/BiteBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteBeatApp.swift
+//  BiteBeat
+//
+//  Created by Windy Claudia Napitupulu on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BiteBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
